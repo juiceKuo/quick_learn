755b0d4c7baa3af80be4612d3c888da2b3f87c36
Charlie Kuo <berbejuice@gmail.com>
2019-08-07 00:11:35 +0800

    class variable.

--- OOP_Tutorial_1.py
+++ OOP_Tutorial_1.py
@@ -9,6 +9,10 @@ Created on Wed Jul 17 22:47:28 2019
 
 class Employee:
     
+    num_of_empls = 0
+    raise_amount = 1.04
+    
+    
     def __init__(self, first, last, pay):
         
         self.first = first
@@ -16,8 +20,14 @@ class Employee:
         self.pay = pay
         self.email = first + '.' + last + '@company.com'
         
+        Employee.num_of_empls += 1
+        
+        
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+    
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
         
     
         
@@ -28,3 +38,24 @@ print(emp_1.email)
 print(emp_2.email)
 
 print(emp_1.fullname())
+
+print(emp_1.pay)
+emp_1.apply_raise()
+print(emp_1.pay)
+
+Employee.raise_amount = 1.05
+
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
+
+print(Employee.num_of_empls)
+
+
+
+
+
+
+
+
+
